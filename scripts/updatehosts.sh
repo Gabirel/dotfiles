bbcb6a5dd5cf0f53dcaf447c8f31f3c8b0a14a3c
Gabriel <wqq977sz@163.com>
2017-07-13 14:13:20 +0800

    Correct wrong word

--- scripts/updatehosts.sh
+++ scripts/updatehosts.sh
@@ -3,7 +3,7 @@ if [ `id -u` -eq 0 ]; then
     wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O /tmp/hosts
     sleep 1
     
-    echo "remving /etc/hosts"
+    echo "removing /etc/hosts"
     rm /etc/hosts
     cp /tmp/hosts /etc/hosts
     rm /tmp/hosts
