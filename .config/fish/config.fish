d5b3eae157e100144b2ac9aaa64bc134c8922972
Gabriel <wqq977sz@163.com>
2019-03-21 21:52:41 +0800

    Add task-related command into alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
-set PATH /home/gabriel/.nexustools $PATH
-set PATH /home/gabriel/.gem/ruby/2.4.0/bin $PATH
 thefuck --alias | source
+alias t="task"
+alias ta="task add"
+alias td="task done"
+alias t-="task delete"
