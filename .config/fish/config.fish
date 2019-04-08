2930f129c3bd54c105dc0bd71da339cabde9f962
Gabriel <wqq977sz@163.com>
2019-04-08 09:58:49 +0800

    Add macOS config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,8 @@ alias tan="task annotate"
 alias td="task done"
 alias to="taskopen"
 alias t-="task delete"
+
+
+# On MacOS
+set HOMEBREW_BOTTLE_DOMAIN https://mirrors.aliyun.com/homebrew/homebrew-bottles
+alias git="env LANG=en_GB git"
