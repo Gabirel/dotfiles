a8c503b9b76ece205d1fc1c03439d51ebb85d9c9
Gabirel <wqq977sz@163.com>
2019-12-23 15:03:16 +0800

    Add cmakedebug & cmakerelease

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,12 @@ set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
 source ~/.linuxify
 # For Rust
 source $HOME/.cargo/env
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+
+# For CMake
+alias cmakedebug='cmake $1 -DCMAKE_BUILD_TYPE=Debug'
+alias cmakerelease='cmake $1 -DCMAKE_BUILD_TYPE=Release'
 
 alias git="env LANG=en_GB git"
 alias ls="ls --color -h --group-directories-first"
