9e8bd7da4c254678db38ab8c3d74a072a1af5882
Gabirel <wqq977sz@163.com>
2020-01-26 14:56:10 +0800

    Use testing functons in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,35 @@
-thefuck --alias | source
-alias t="task"
-alias ta="task add"
-alias tan="task annotate"
-alias td="task done"
-alias to="taskopen"
-alias t-="task delete"
+# use exa for the good
+if command -s exa > /dev/null
+    alias exa="exa --group-directories-first"
+    alias l="exa"
+    alias ls="exa"
+    alias la="exa -a"
+    alias ll="exa -l"
+    alias lg="exa -l --git --header"
+    alias lt="exa -T"
+    alias lla="exa -la"
+    alias llt="exa -lT"
+    alias lat="exa -aT"
+    alias llat="exa -aT"
+else
+    alias ls="ls --color=always -h --group-directories-first"
+    alias lt="ls --human-readable --size -1 -S --classify"
+end
 
+# import thefuck
+if command -s thefuck > /dev/null
+    thefuck --alias | source
+end
+
+# import task
+if command -s task > /dev/null
+    alias t="task"
+    alias ta="task add"
+    alias tan="task annotate"
+    alias td="task done"
+    alias to="taskopen"
+    alias t-="task delete"
+end
 
 # MacOS Start
 set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
@@ -20,7 +44,5 @@ alias cmakedebug='cmake $1 -DCMAKE_BUILD_TYPE=Debug'
 alias cmakerelease='cmake $1 -DCMAKE_BUILD_TYPE=Release'
 
 alias git="env LANG=en_GB git"
-alias ls="ls --color -h --group-directories-first"
-alias lt="ls --human-readable --size -1 -S --classify"
 alias pcs="proxychains4"
 alias cpv='rsync -ah --info=progress2'
