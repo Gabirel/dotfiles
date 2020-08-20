10af8aeb54c68bb442276c8d5314a9b78effc74f
Gabirel <wqq977sz@163.com>
2020-08-20 17:08:37 +0800

    Add script: auto update v2raydat

---
+++ scripts/update-v2raydat.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This script is modified based on https://github.com/Loyalsoldier/v2ray-rules-dat/issues/10#issuecomment-593858015
+
+set -e
+
+# only works for v2rayU
+v2ray_folder="/Applications/V2rayU.app/Contents/Resources/v2ray-core/"
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
+
+echo -e "${GREEN}>>> change directory...${NC}"
+cd $v2ray_folder
+
+echo -e "${GREEN}>>> delete old dat files...${NC}"
+rm -f geoip.dat geosite.dat
+
+echo -e "${GREEN}>>> downloading geoip.dat files...${NC}"
+curl -L -O $GEOIP_URL
+
+echo -e "${GREEN}>>> downloading geosite.dat files...${NC}"
+curl -L -O $GEOSITE_URL
+
+echo -e "${GREEN}Finished!!${NC}"
