3783f6a09b7826f78666feb0c9b271741d5c4738
Gabirel <wqq977sz@163.com>
2021-12-15 11:05:26 +0800

    Remove assets later to make sure service works properly

--- scripts/update-v2raydat-vps.sh
+++ scripts/update-v2raydat-vps.sh
@@ -6,45 +6,58 @@ set -e
 # only works for linux
 v2ray_folder="/usr/local/share/xray/"
 
+YELLOW='\033[33m'
 GREEN='\033[0;32m'
+RedBG='\033[41;37m'
+GreenBG='\033[42;37m'
 NC='\033[0m'
 
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-CN_GEOIP_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat"
-CN_GEOSITE_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat"
+GEOIP="geoip.dat"
+GEOSITE="geosite.dat"
 
 
-echo -e "${GREEN}>>> change directory...${NC}"
-cd $v2ray_folder
-
-echo -e "${GREEN}>>> delete old dat files...${NC}"
-rm -f geoip.dat geosite.dat
-retVal=$?
-if [ $retVal -eq 1 ]; then
-    echo "failed to remove geoip and geosite"
+# arguments must be <empty> or "cn"
+if [[ $# -gt 1 ]]; then
+    echo -e "${RedBG}>>> only accept 1 argument!${NC}"
     exit 1
 fi
 
-echo -e "${GREEN}>>> downloading geoip.dat files...${NC}"
-curl -L -O $GEOIP_URL
-retVal=$?
-if [ $retVal -eq 1 ]; then
-    curl -L -O $CN_GEOIP_URL
+# validation check
+if [[ $1 == "cn" ]]; then
+    # set different URL for downloading assets
+    GEOIP_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat"
+    GEOSITE_URL="https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat"
+elif [[ $# -eq 1 && $1 != "cn" ]]; then
+    echo -e "${RedBG}>>> arguments only accept \"cn\"!${NC}"
+    exit 1
 fi
 
+# 1. Start downloading
+echo -e "${GREEN}>>> downloading geoip.dat files...${NC}"
+echo -e "${YELLOW}geoip URL: $GEOIP_URL${NC}"
+curl -L $GEOIP_URL --output /tmp/$GEOIP
 
 echo -e "${GREEN}>>> downloading geosite.dat files...${NC}"
-curl -L -O $GEOSITE_URL
-retVal=$?
-if [ $retVal -eq 1 ]; then
-    curl -L -O $CN_GEOSITE_URL
-fi
+echo -e "${YELLOW}geosite URL: $GEOSITE_URL${NC}"
+curl -L $GEOSITE_URL --output /tmp/$GEOSITE
+
+# 2. Clean old assets
+echo -e "${GREEN}>>> delete old dat files...${NC}"
+rm -f $v2ray_folder/$GEOIP $v2ray_folder/$GEOSITE
+
+# 3. Replace old assets
+echo -e "${GREEN}>>> Replacing new geoip/geosite...${NC}"
+mv /tmp/$GEOIP $v2ray_folder/
+mv /tmp/$GEOSITE $v2ray_folder/
+
+echo -e "${GREEN}Finished!!${NC}"
 
 echo -e "${GREEN}Finished for geoip/geosite!${NC}"
 
-# restart server
+# 4. Restart server
 echo -e "${GREEN}>>> Restart xray servers..${NC}"
 systemctl restart xray
 echo -e "${GREEN}All Finished!!${NC}"
