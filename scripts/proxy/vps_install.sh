85fba6aae30096dc1055d811897d9a2448000861
Gabriel <wqq977sz@163.com>
2024-02-17 17:41:20 +0800

    Fix shell syntax error

--- scripts/proxy/vps_install.sh
+++ scripts/proxy/vps_install.sh
@@ -33,13 +33,13 @@ pre_install() {
 # install latest nginx
 install_nginx() {
     # 1. install all needed tools
-    apt install -y gnupg2 ca-certificates lsb-release debian-archive-keyring \ 
-        && curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg \ 
-        && echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list \ 
-        && echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" > /etc/apt/preferences.d/99nginx \ 
-        && apt update -y && apt install -y nginx \ 
-        && mkdir -p /etc/systemd/system/nginx.service.d \ 
-        && echo -e "[Service]\nExecStartPost=/bin/sleep 0.1" > /etc/systemd/system/nginx.service.d/override.conf \ 
+    apt install -y gnupg2 ca-certificates lsb-release debian-archive-keyring \
+        && curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg \
+        && echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list \
+        && echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" > /etc/apt/preferences.d/99nginx \
+        && apt update -y && apt install -y nginx \
+        && mkdir -p /etc/systemd/system/nginx.service.d \
+        && echo -e "[Service]\nExecStartPost=/bin/sleep 0.1" > /etc/systemd/system/nginx.service.d/override.conf \
         && systemctl daemon-reload
 }
 
